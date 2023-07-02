d7e5d136dee6f94197270b28f64a2a07f67de1e8
zukicode <zukicode@gmail.com>
2023-07-02 21:01:02 +0300

    predeploy for vercel

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename='plan-craft'>
+		<BrowserRouter>
 			<Provider store={store}>
 				<App />
 			</Provider>
